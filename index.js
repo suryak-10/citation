7b2ce4246c8bf062fa670bbd7340043cdc8fa36a
suryak-10 <ayrus4444@gmail.com>
2024-12-14 10:56:07 +0530

    chore

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const Cite = require('citation-js')
+const Cite = require('citation-js');
+const { ieee } = require('./csl/ieee');
+const { chicagoFullNote } = require('./csl/chicagoFullNote');
+const apa = require('./csl/apa');
 
-Cite.plugins.add('ref', {
-    output: {
-        type: (...args) => console.log(args)
+let config = Cite.plugins.config.get('@csl')
+config.templates.add("style", apa);
+config.templates.add("chicagoFullNote", chicagoFullNote);
+
+const safeHtmlFormatter = ({ value, format }) => {
+    if (format == 'NormalToSafe') {
+        return value.replaceAll('<', '‹').replaceAll('>', '›');
+    } else {
+        return value.replaceAll('‹', '<').replaceAll('›', '>');
     }
-})
-
+}
 
 const data1 = [
     {
@@ -43,29 +51,66 @@ const data2 = {
     "given": "S.",
     "x": "and ",
     "year": "(2006). ",
+    "title": "This is surya's title",
     "source": "Profile of undergraduates in U.S. Postsecondary education Institutions: 2003–04: With a Special Analysis of Community College Students (NCES 2006-184). ",
     "publisher-name": "National  Center for Education Statistics, U.S. Department of Education: ",
     "publisher-loc": "Washington, DC:",
     "publisher": "National  Center for Education Statistics, U.S. Department of Education: ",
     "publisher-place": "Washington, DC:",
     "container-title": "html",
-    "issued": {
-        "date-parts": [
-            {
-                "year": 2006,
-                "day": 10,
-                "month": 10
-            }
-        ]
-    },
+    issued: { "date-parts": [[1957, 1, 1]] },
     "label": '[12]',
+    "type": "bill",
+    "organizer": ["Collab"],
 }
 
-const reference = new Cite(data1);
+const data3 = {
+    "type": "article",
+    "container-title": "City of Seattle, SEA Department of Neighborhoods, City of Seattle will transfer Fire Station 6 to community. Front Porch. ",
+    "title": "https://frontporceh.seadttle.gov/2020/06/12/city-of-seattle-will-transfer-fire-station-6-to-community/",
+    "issued": {
+        "date-parts": [
+            [
+                2020
+            ]
+        ]
+    },
+    "author": [],
+    "original-publisher-place": "original-place"
+}
 
-const output = reference.format("bibliography", {
-    template: 'apa',
-    format: 'html',
+const data4 = {
+    "type": "article-journal",
+    "author": [
+        {
+            "family": "Aulagnier",
+            "given": "P."
+        }
+    ],
+    "title": "Birth of a body: Origin of a history.",
+    "container-title": "International article-journal of Psychoanaly",
+    "volume": "96",
+    "issue": "5",
+    "page": "1371-1401",
+    "issued": [
+        {
+            "date-parts": [
+                "2015"
+            ]
+        }
+    ]
+}
+
+
+
+const reference1 = new Cite(data4);
+
+const output = reference1.format("bibliography", {
+    template: 'chicagoFullNote',
+    format: 'text',
 })
 
-console.log(output)
+console.log({ output });
+
+// console.log(safeHtmlFormatter({value:output, format: 'SafeToNormal'}));
+
